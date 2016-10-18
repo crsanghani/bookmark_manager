9a8f6a8da8a6cc26ac05049d8377b7bdd360bc9a
Chetan Sanghani <crsanghani@gmail.com>
2016-10-18 11:20:59 +0100

    made first feature test pass

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,17 @@
 require 'rubygems'
 require 'sinatra/base'
+require_relative 'models/link'
+
 
 class BookmarkManager < Sinatra::Base
 
-
   get '/' do
-    
+    redirect '/links'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
