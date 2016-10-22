9f05e5a635c95dd866bcb5021f54044521a22b92
Chetan Sanghani <crsanghani@gmail.com>
2016-10-22 14:54:20 +0100

    tests passing rake file created

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,10 @@
-require 'rubygems'
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
+
+require_relative 'data_mapper_setup'
 require_relative 'models/link'
 
-ENV['RACK_ENV'] ||= 'development'
 
 class BookmarkManager < Sinatra::Base
 
@@ -20,8 +22,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title], tags: params[:tags])
-    redirect '/links'
+     link = Link.create(url: params[:url], title: params[:title])
+     params[:tags].split.each do |tag|
+       link.tags << Tag.create(name: tag)
+     end
+     link.save
+     redirect to('/links')
+   end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
+require './app/app'
 require 'rubygems'
 require File.join(File.dirname(__FILE__), './app/app.rb')
 
