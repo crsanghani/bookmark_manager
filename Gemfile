56e70e27135d8819b99ea86e8ce5e3631bbf7992
Chetan Sanghani <crsanghani@gmail.com>
2016-10-22 18:29:19 +0100

    sinatra/flash added

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pry'
 gem 'sinatra'
 gem 'rake'
 gem 'bcrypt'
+gem 'sinatra-flash'
 
 group :test do
   gem 'rspec'
--- app/app.rb
+++ app/app.rb
@@ -1,13 +1,14 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
+require 'sinatra/flash'
 
 require_relative 'data_mapper_setup'
 require_relative 'models/link'
 require_relative 'models/user'
 
 class BookmarkManager < Sinatra::Base
-
+register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
@@ -40,15 +41,25 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create( email: params[:email],
-                        password: params[:password],
-                        password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    # we just initialize the object
+    # without saving it. It may be invalid
+    @user = User.new(email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
+    if @user.save # #save returns true/false depending on whether the model is successfully saved to the database.
+      session[:user_id] = @user.id
+      redirect to('/')
+      # if it's not valid,
+      # we'll render the sign up form again
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
